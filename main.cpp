b4e9a51a3a3e3136dd8d39e157b916a493366887
Jeremiah Munson <j.munson225@gmail.com>
2019-02-26 07:40:19 -0600

    Created outline for multiply.

--- main.cpp
+++ main.cpp
@@ -7,4 +7,23 @@ int main()
     return 0;
 }
 
-//new functions go here
+bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+{
+    // FIND CHARACTERISTIC
+    int n = n1*n2;
+    int d = d1*d2;
+    int characteristic = c1*c2 + n/d;
+
+    // TURN CHARACTERISITIC INTO CHAR ARRAY (STRING)
+    // Find length of characterisitc
+    int characterisiticLength = 0;
+    // Find each digit in characterisitic
+    // Place digits as characters into 'result'
+
+    // FIND DIGITS IN DECIMAL PLACE
+    // Place decimal point as character into result
+    // Place digits as characters into result
+
+    
+    return true;
+}
