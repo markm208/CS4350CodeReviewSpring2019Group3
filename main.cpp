fe80ac524705fea0d733646f27c25cbf0bfc2989
TheRealJKells <jkeller1@carthage.edu>
2019-02-26 10:07:16 -0600

    added starter code

--- main.cpp
+++ main.cpp
@@ -2,9 +2,38 @@
 
 using namespace std;
 
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len); 
+
 int main()
 {
+    char answer[10];
+    int c1, n1, d1;
+    int c2, n2, d2;
+    
+    c1 = 1;
+    n1 = 1;
+    d1 = 2;
+    
+    c2 = 2;
+    n2 = 2;
+    d2 = 3; 
+    
+    //if the C string could hold at least the characteristic
+    if (subtract(c1, n1, d1, c2, n2, d2, answer, 10))
+    {
+        //display string with answer 4.1666666
+    }
+    else
+    {
+        //display error message
+    }
     return 0;
 }
 
 //new functions go here
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+{
+
+}
+//
+//
