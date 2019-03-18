adff7497a77f1a0877344babe7fdd36baa6273fa
TheRealJKells <jkeller1@carthage.edu>
2019-03-18 15:27:54 -0500

    deleted more unneccesary code

--- main.cpp
+++ main.cpp
@@ -14,74 +14,11 @@ void testSubtract();
 
 int main()
 {
-	//characteristic and mantissa test
-	//testCharacteristicAndMantissa();
-	
 	//math function tests
 	testMath();
-
     return 0;
 }
-//--
-// void testCharacteristicAndMantissa()
-// {
-// 	shouldConvert("123.456", 123, 456, 1000);
-// 	shouldConvert("    123.456", 123, 456, 1000);
-// 	shouldConvert("123.456    ", 123, 456, 1000);
-// 	shouldConvert("    123.456    ", 123, 456, 1000);
 
-// 	shouldConvert("+123.456", 123, 456, 1000);
-// 	shouldConvert("   +123.456", 123, 456, 1000);
-// 	shouldConvert("+123.456   ", 123, 456, 1000);
-// 	shouldConvert("   +123.456   ", 123, 456, 1000);
-
-// 	shouldConvert("-123.456", -123, 456, 1000);
-// 	shouldConvert("   -123.456", -123, 456, 1000);
-// 	shouldConvert("-123.456   ", -123, 456, 1000);
-// 	shouldConvert("    -123.456   ", -123, 456, 1000);
-
-// 	shouldConvert("0.456", 0, 456, 1000);
-// 	shouldConvert("   0.456", 0, 456, 1000); 
-// 	shouldConvert("0.456   ", 0, 456, 1000);
-// 	shouldConvert("   0.456   ", 0, 456, 1000);
-
-// 	shouldConvert("-0.456", 0, -456, 1000);
-// 	shouldConvert("   -0.456", 0, -456, 1000);
-// 	shouldConvert("-0.456   ", 0, -456, 1000);
-// 	shouldConvert("   -0.456   ", 0, -456, 1000);
-
-// 	shouldConvert(".456", 0, 456, 1000);
-// 	shouldConvert("    .456", 0, 456, 1000);
-// 	shouldConvert(".456   ", 0, 456, 1000);
-// 	shouldConvert("   .456   ", 0, 456, 1000);
-
-// 	shouldConvert("-.456", 0, -456, 1000);
-// 	shouldConvert("    -.456", 0, -456, 1000);
-// 	shouldConvert("-.456   ", 0, -456, 1000);
-// 	shouldConvert("   -.456   ", 0, -456, 1000);
-
-// 	shouldConvert("123456", 123456, 0, 10);
-// 	shouldConvert("   123456", 123456, 0, 10);
-// 	shouldConvert("123456   ", 123456, 0, 10);
-// 	shouldConvert("   123456   ", 123456, 0, 10);
-
-// 	shouldConvert("-123456", -123456, 0, 10);
-// 	shouldConvert("   -123456", -123456, 0, 10);
-// 	shouldConvert("-123456   ", -123456, 0, 10);
-// 	shouldConvert("   -123456   ", -123456, 0, 10);
-
-// 	shouldConvert("000123.456", 123, 456, 1000);
-// 	shouldConvert("123.45600000", 123, 456, 1000);
-// 	shouldConvert("00000123.45600000", 123, 456, 1000);
-
-// 	shouldConvert("-000123.456", -123, 456, 1000);
-// 	shouldConvert("-123.45600000", -123, 456, 1000);
-// 	shouldConvert("-00000123.45600000", -123, 456, 1000);
-
-// 	shouldConvert("123.00000456", 123, 456, 100000000);
-// 	shouldConvert("-123.00000456", -123, 456, 100000000);
-// }
-//--
 void shouldConvert(char number[], int expectedCharacteristic, int expectedNumerator, int expectedDenominator)
 {
 	int c, n, d;
@@ -139,77 +76,10 @@ void shouldNotConvert(char number[])
 //--
 void testMath()
 {
-	//add
-	testAdd();
-	testSubtract();
-	testMultiply();
-	testDivide();
+	testSubtract();	
 }
 //--
-void testAdd()
-{
-	const int SHORT_ARRAY_LENGTH = 5;
-	char shortArray[SHORT_ARRAY_LENGTH];
 
-	const int MEDIUM_ARRAY_LENGTH = 10;
-	char mediumArray[MEDIUM_ARRAY_LENGTH];
-
-	const int LARGE_ARRAY_LENGTH = 20;
-	char largeArray[LARGE_ARRAY_LENGTH];
-
-	//should not be enough space in the array for the result
-	if (add(INT_MAX, 0, 10, INT_MAX, 0, 10, shortArray, SHORT_ARRAY_LENGTH))
-	{
-		cout << "Error: not enough space in array" << endl;
-	}
-
-	//0 + 0 = "0"
-	add(0, 0, 10, 0, 0, 10, shortArray, SHORT_ARRAY_LENGTH);
-	shouldConvert(shortArray, 0, 0, 10);
-	add(0, 0, 10, 0, 0, 10, mediumArray, MEDIUM_ARRAY_LENGTH);
-	shouldConvert(mediumArray, 0, 0, 10);
-	add(0, 0, 10, 0, 0, 10, largeArray, LARGE_ARRAY_LENGTH);
-	shouldConvert(largeArray, 0, 0, 10);
-
-	//1 + 1 = "2"
-	add(1, 0, 10, 1, 0, 10, shortArray, SHORT_ARRAY_LENGTH);
-	shouldConvert(shortArray, 2, 0, 10);
-	add(1, 0, 10, 1, 0, 10, mediumArray, MEDIUM_ARRAY_LENGTH);
-	shouldConvert(mediumArray, 2, 0, 10);
-	add(1, 0, 10, 1, 0, 10, largeArray, LARGE_ARRAY_LENGTH);
-	shouldConvert(largeArray, 2, 0, 10);
-
-	//1 + -1.5 = "-.5"
-	add(1, 0, 10, -1, 1, 2, shortArray, SHORT_ARRAY_LENGTH);
-	shouldConvert(shortArray, 0, -5, 10);
-	add(1, 0, 10, -1, 1, 2, mediumArray, MEDIUM_ARRAY_LENGTH);
-	shouldConvert(mediumArray, 0, -5, 10);
-	add(1, 0, 10, -1, 1, 2, largeArray, LARGE_ARRAY_LENGTH);
-	shouldConvert(largeArray, 0, -5, 10);
-
-	//1.125 + 1.6R = "2.79"
-	add(1, 1, 8, 1, 2, 3, shortArray, SHORT_ARRAY_LENGTH);
-	shouldConvert(shortArray, 2, 79, 100);
-
-	//1.125 + 1.6R = "2.7916666"
-	add(1, 1, 8, 1, 2, 3, mediumArray, MEDIUM_ARRAY_LENGTH);
-	shouldConvert(mediumArray, 2, 7916666, 10000000);
-
-	//1.125 + 1.6R = "2.79166666666666666"
-	add(1, 1, 8, 1, 2, 3, largeArray, LARGE_ARRAY_LENGTH);
-	//can't use the convert function because the num/denom would overflow
-	char expectedResult[] = "2.79166666666666666";
-	for (int i = 0; i < LARGE_ARRAY_LENGTH; i++)
-	{
-		if (expectedResult[i] != largeArray[i])
-		{
-			cout << "Error: mismatch in C strings in add()." << endl
-				<< "Expected: " << expectedResult << " "
-				<< "Actual: " << largeArray
-				<< endl;
-		}
-	}
-}
 //--
 void testSubtract()
 {
@@ -276,60 +146,7 @@ void testSubtract()
 	}
 }
 //--
-void testMultiply()
-{
-	const int SHORT_ARRAY_LENGTH = 5;
-	char shortArray[SHORT_ARRAY_LENGTH];
 
-	const int MEDIUM_ARRAY_LENGTH = 10;
-	char mediumArray[MEDIUM_ARRAY_LENGTH];
-
-	const int LARGE_ARRAY_LENGTH = 20;
-	char largeArray[LARGE_ARRAY_LENGTH];
-
-	//should not be enough space in the array for the result
-	if (multiply(INT_MAX, 0, 10, INT_MAX, 0, 10, shortArray, SHORT_ARRAY_LENGTH))
-	{
-		cout << "Error: not enough space in array" << endl;
-	}
-
-	//0 * 0 = "0"
-	multiply(0, 0, 10, 0, 0, 10, shortArray, SHORT_ARRAY_LENGTH);
-	shouldConvert(shortArray, 0, 0, 10);
-	multiply(0, 0, 10, 0, 0, 10, mediumArray, MEDIUM_ARRAY_LENGTH);
-	shouldConvert(mediumArray, 0, 0, 10);
-	multiply(0, 0, 10, 0, 0, 10, largeArray, LARGE_ARRAY_LENGTH);
-	shouldConvert(largeArray, 0, 0, 10);
-
-	//3 * 2 = "6"
-	multiply(3, 0, 10, 2, 0, 10, shortArray, SHORT_ARRAY_LENGTH);
-	shouldConvert(shortArray, 6, 0, 10);
-	multiply(3, 0, 10, 2, 0, 10, mediumArray, MEDIUM_ARRAY_LENGTH);
-	shouldConvert(mediumArray, 6, 0, 10);
-	multiply(3, 0, 10, 2, 0, 10, largeArray, LARGE_ARRAY_LENGTH);
-	shouldConvert(largeArray, 6, 0, 10);
-
-	//3 * -1.5 = "-4.5"
-	multiply(3, 0, 10, -1, 1, 2, shortArray, SHORT_ARRAY_LENGTH);
-	shouldConvert(shortArray, -4, 5, 10);
-	multiply(3, 0, 10, -1, 1, 2, mediumArray, MEDIUM_ARRAY_LENGTH);
-	shouldConvert(mediumArray, -4, 5, 10);
-	multiply(3, 0, 10, -1, 1, 2, largeArray, LARGE_ARRAY_LENGTH);
-	shouldConvert(largeArray, -4, 5, 10);
-
-	//1.125 * 1.6R = "1.87"
-	multiply(1, 1, 8, 1, 2, 3, shortArray, SHORT_ARRAY_LENGTH);
-	shouldConvert(shortArray, 1, 87, 100);
-
-	//1.125 * 1.6R = "1.875"
-	multiply(1, 1, 8, 1, 2, 3, mediumArray, MEDIUM_ARRAY_LENGTH);
-	shouldConvert(mediumArray, 1, 875, 1000);
-
-	//1.125 * 1.6R = "1.875"
-	multiply(1, 1, 8, 1, 2, 3, largeArray, LARGE_ARRAY_LENGTH);
-	shouldConvert(largeArray, 1, 875, 1000);
-}
-//--
 
 
 //new functions go here
@@ -450,5 +267,4 @@ bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int
 		result[0] = '-';
 	}
 	return retVal;
-	
 }
