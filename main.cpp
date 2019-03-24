c36a314537b50bc1090a5251c88128c8681537c5
mhernandez3 <mahernandez33333@gmail.com>
2019-03-24 13:58:25 -0500

    works, except with non base power ten

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+
+// add if not base divisable by 10
+int adding_d(int n1,int d1,int n2,int d2)
+{
+    int d3=0;
+    d3=d1*d2;
+    return d3;
+}
+int adding_n(int n1,int d1,int n2,int d2)
+{
+    int n3=0;
+    n3=(n1*d2)+(n2*d2);
+    return n3;
+}
 //get the length of an integer.
 int lengthof(int integer)
 {
@@ -14,42 +28,15 @@ int lengthof(int integer)
     }
     return length;
 }
-//set up divisor to hightest divisor
-int divisor(int d1,int d2,int &n1,int &n2)
-{
-    if(d1 == d2)
-    {
-        return d1;
-    }
-    else
-    {
-        while(1)
-        {
-            if(d1>d2)
-            {
-                d2=d2*10;
-                n2=n2*10;
-                if(d1==d2)
-                {
-                    return d1;
-                }
-            }
-            else
-            {
-                d1=d1*10;
-                n1=n1*10;
-                if(d1==d2)
-                {
-                    return d1;
-                }
-            }
-            
-        }
-    }
-}
+
 //translate integer to char array
 void translate_to_string(int integer,char* resultV)
 {
+    if(integer==0)
+    {
+        resultV[0]='0';
+    }
+    else{
     int converter=1;
     int length=0;
     while(integer>=converter)
@@ -64,7 +51,7 @@ void translate_to_string(int integer,char* resultV)
     for(int i = length;i>=0;i--)
     {
         int local_int=0;
-        while(integer>=converter)
+        while(integer>=converter-1)
         {
             local_int=local_int+1;
             integer=integer-converter;
@@ -81,7 +68,8 @@ void translate_to_string(int integer,char* resultV)
     {
         resultV[i]=result[i];
     }
-    //convert to pointer
+    cout<<resultV<<endl;
+    }
 }
 //add function
 bool add(int c1,int n1, int d1,int c2,int n2, int d2, char* result,int len)
@@ -96,10 +84,10 @@ bool add(int c1,int n1, int d1,int c2,int n2, int d2, char* result,int len)
         n1=-n1;
     }
     //set up answer devisor and set up numerator as well
-    int d3 = divisor(d1,d2,n1,n2);
+    int d3 = adding_d(n1,d1,n2,d2);
 
     //add numerators
-    int n3=n1+n2;
+    int n3=adding_n(n1,d1,n2,d2);
     //add integers
     int c3=c1+c2;
     //add, subtract, or do nothing to characteristic
@@ -124,6 +112,10 @@ bool add(int c1,int n1, int d1,int c2,int n2, int d2, char* result,int len)
     translate_to_string(n3,char_n3);
 
     //loop to add to characteristic and numerator to result
+    if(lengthof(c3)+lengthof(n3)+1>len)
+    {
+        return false;
+    }
     int i=0;
     while(i!=len_c3)
     {
@@ -144,25 +136,76 @@ bool add(int c1,int n1, int d1,int c2,int n2, int d2, char* result,int len)
         result[i]=char_n3[i-(len_c3+1)];
         i++;
     }
-
+    result[i]='\0';
     return true;
 }
 
 int main()
 {
-    //set up char array
-    char result[10];
-    //add values
-    add(59967,6,10,-28,74,100,result,10);
-    int i = 0;
-    //print results
-    cout<<"your result is :";
-    while(result[i]<=57 &&result[i]>=48||result[i]=='.')
-    {
-        cout<<result[i];
-        i++;
-    }
-    cout<<endl;
+	int SHORT_ARRAY_LENGTH = 5;
+	char shortArray[SHORT_ARRAY_LENGTH];
+
+	int MEDIUM_ARRAY_LENGTH = 10;
+	char mediumArray[MEDIUM_ARRAY_LENGTH];
+
+	int LARGE_ARRAY_LENGTH = 20;
+	char largeArray[LARGE_ARRAY_LENGTH];
+
+	//should not be enough space in the array for the result
+	if (add(INT8_MAX, 0, 10, INT8_MAX, 0, 10, shortArray, SHORT_ARRAY_LENGTH))
+	{
+		cout << "Error: not enough space in array" << endl;
+	}
+
+	//0 + 0 = "0"
+	add(0, 0, 10, 0, 0, 10, shortArray, SHORT_ARRAY_LENGTH);
+    cout<<shortArray<<endl;
+////shouldConvert(shortArray, 0, 0, 10);
+	add(0, 0, 10, 0, 0, 10, mediumArray, MEDIUM_ARRAY_LENGTH);
+    cout<<mediumArray<<endl;
+	//shouldConvert(mediumArray, 0, 0, 10);
+	add(0, 0, 10, 0, 0, 10, largeArray, LARGE_ARRAY_LENGTH);
+    cout<<mediumArray<<endl;
+	//shouldConvert(largeArray, 0, 0, 10);
+
+	//1 + 1 = "2"
+	add(1, 0, 10, 1, 0, 10, shortArray, SHORT_ARRAY_LENGTH);
+    cout<<shortArray<<endl;
+	add(1, 0, 10, 1, 0, 10, mediumArray, MEDIUM_ARRAY_LENGTH);
+    cout<<mediumArray<<endl;
+	add(1, 0, 10, 1, 0, 10, largeArray, LARGE_ARRAY_LENGTH);
+    cout<<largeArray<<endl;
+	//1 + -1.5 = "-.5"
+    cout<<shortArray<<endl;
+	add(1, 0, 10, -1, 1, 2, shortArray, SHORT_ARRAY_LENGTH);
+    cout<<shortArray<<endl;
+	add(1, 0, 10, -1, 1, 2, mediumArray, MEDIUM_ARRAY_LENGTH);
+    cout<<shortArray<<endl;
+	add(1, 0, 10, -1, 1, 2, largeArray, LARGE_ARRAY_LENGTH);
+
+	//1.125 + 1.6R = "2.79"
+    cout<<shortArray<<endl;
+	add(1, 1, 8, 1, 2, 3, shortArray, SHORT_ARRAY_LENGTH);
+
+	//1.125 + 1.6R = "2.7916666"
+    cout<<shortArray<<endl;
+	add(1, 1, 8, 1, 2, 3, mediumArray, MEDIUM_ARRAY_LENGTH);
+
+	//1.125 + 1.6R = "2.79166666666666666"
+    cout<<shortArray<<endl;
+	add(1, 1, 8, 1, 2, 3, largeArray, LARGE_ARRAY_LENGTH);
+	//can't use the convert function because the num/denom would overflow
+	char expectedResult[] = "2.79166666666666666";
+	for (int i = 0; i < LARGE_ARRAY_LENGTH; i++)
+	{
+		if (expectedResult[i] != largeArray[i])
+		{
+			cout << "Error: mismatch in C strings in add()." << endl
+				<< "Expected: " << expectedResult << " "
+				<< "Actual: " << largeArray
+				<< endl;
+		}
+	}
     return 0;
 }
 
